d66b4341bd909bfdca8cd79fc0812fee9dd65a72
ferg0311 <69618092+ferg0311@users.noreply.github.com>
2022-08-19 21:16:04 -0600

    Add files via upload

---
+++ src/screen/Pokemon.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { ParamListBase } from "@react-navigation/routers";
+import { PokemonEntity } from "../util/types/pokemon";
+import Header from "../components/pokemon/Header";
+import Types from "../components/pokemon/Types";
+import Details from "../components/pokemon/Details";
+import { ScrollView } from "react-native-gesture-handler";
+import useAuth from "../hooks/useAuth";
+import FavoriteButton from "../components/pokemon/FavoriteButton";
+
+export default function PokemonScreen({
+  route,
+  navigation,
+}: NativeStackScreenProps<ParamListBase>) {
+  const pokemon: PokemonEntity = route.params! as PokemonEntity;
+
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    navigation.setOptions({
+  }, [navigation, pokemon, auth]);
+
+  return (
+    <ScrollView>
+      <Types types={pokemon.types} />
+      <Details pokemon={pokemon} />
+    </ScrollView>
+  );
+}
